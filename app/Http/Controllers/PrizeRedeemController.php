e01f9d9ebd8e3aedc280d45f0986d911eb3baa93
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 00:17:21 -0300

    Added basic methods to controllers

--- app/Http/Controllers/PrizeRedeemController.php
+++ app/Http/Controllers/PrizeRedeemController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PrizeRedeemRequest;
+use App\Services\PrizeRedeemService;
+use Symfony\Component\HttpFoundation\Response;
 
-class PrizeRedeemController extends Controller
+class PrizeRedeemController extends BaseController
 {
-    //
+    public function __construct(PrizeRedeemService $service)
+	{
+		parent::__construct($service);
+	}
+
+	public function index()
+	{
+		return $this->defaultResponse($this->service->getAllRecords());
+	}
+
+	public function store(PrizeRedeemRequest $request)
+	{
+		return $this->defaultResponse($this->service->saveRecord($request->getData()), Response::HTTP_CREATED);
+	}
+
+	public function show(string $id)
+	{
+		return $this->defaultResponse($this->service->findRecord($id));
+	}
+
+	public function update(PrizeRedeemRequest $request, string $id)
+	{
+		return $this->defaultResponse($this->service->updateRecord($id, $request->getData()), Response::HTTP_CREATED);
+	}
 }
