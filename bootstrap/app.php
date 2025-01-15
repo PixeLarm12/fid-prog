0e9f26c4047cbe4f22d8f86ebd587a8337d8ff62
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 00:35:07 -0300

    Added verification to always return as JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Enums\AbstractEnum;
+use App\Util\StringUtil;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        apiPrefix: AbstractEnum::API_ROUTE_PREFIX
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e) {
+            if($e instanceof ValidationException) {
+                return response()->json(
+                        ['message' => StringUtil::getValidationErrorsMessages($e->errors())],
+                        $e->status);
+            }
+        });
     })->create();
