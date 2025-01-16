f4ef4fe55689be219ccf292f646c3df798a3c93f
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 22:41:07 -0300

    Fixed requests

--- app/Http/Requests/PrizeRedeemRequest.php
+++ app/Http/Requests/PrizeRedeemRequest.php
@@ -19,25 +19,25 @@ class PrizeRedeemRequest extends FormRequest
     public function messages() : array
 	{
 		return [
-			'prize_id.required'                 => 'Prize is required',
-			'prize_id.integer'                 => 'Prize must be integer',
-			'user_id.required'                 => 'User is required',
-			'user_id.integer'                 => 'User must be integer',
-			'date.required'                 => 'Date is required',
-			'date.integer'                 => 'Date must be datetime',
-			'redeemed.required'                 => 'Redeemed is required',
-			'redeemed.min'                  => 'Redeemed must have at least 3 characters',
-			'redeemed.max'                  => 'Redeemed cannot be longer than 70 characters',
+			'prize_id.required' => 'Prize is required',
+			'prize_id.integer' => 'Prize must be integer',
+			'user_id.required' => 'User is required',
+			'user_id.integer' => 'User must be integer',
+			'date.required' => 'Date is required',
+			'date.integer' => 'Date must be datetime',
+			'redeemed.required' => 'Redeemed is required',
+			'redeemed.min' => 'Redeemed must have at least 3 characters',
+			'redeemed.max' => 'Redeemed cannot be longer than 70 characters',
 		];
 	}
 
     public function getData() : array
 	{
 		return [
-			'prize_id'                 => $this->input('prize_id'),
-			'user_id'                 => $this->input('user_id'),
-			'date'                 => $this->input('date'),
-			'redeemed'                 => $this->input('redeemed'),
+			'prize_id' => $this->input('prize_id'),
+			'user_id' => $this->input('user_id'),
+			'date' => $this->input('date'),
+			'redeemed' => $this->input('redeemed'),
 		];
 	}
 }
