29ef5249afbee5d4155f6f175b8c84cde398989a
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 22:41:48 -0300

    Added transaction insert

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -3,11 +3,21 @@
 namespace App\Services;
 
 use App\Repositories\TransactionRepository;
+use Illuminate\Database\Eloquent\Model;
 
 class TransactionService extends BaseService
 {
 	public function __construct(TransactionRepository $repository)
 	{
 		parent::__construct($repository);
+	}
+
+	public function saveRecord(array $data) : Model
+	{
+		$transaction = parent::saveRecord($data);
+
+		$transaction->items()->createMany($data['items']);
+
+		return $transaction;
 	}
 }
