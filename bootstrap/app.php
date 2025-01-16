2ae8b589d3aa8902c057f9ae2eb362108bcda35e
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 21:18:52 -0300

    updated exception treatment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,10 +24,34 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Throwable $e) {
+            $return = (object) [
+                'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Some unusual error had occurred'
+            ];
+
             if($e instanceof ValidationException) {
-                return response()->json(
-                        ['message' => StringUtil::getValidationErrorsMessages($e->errors())],
-                        $e->status);
+                $return->message = StringUtil::getValidationErrorsMessages($e->errors());
+                $return->code = $e->status;
+            } else if($e instanceof NotFoundHttpException) {
+                $return->message = $e->getMessage();
+                $return->code = Response::HTTP_NOT_FOUND;
+            } else if($e instanceof BadRequestHttpException) {
+                $return->message = $e->getMessage();
+                $return->code = Response::HTTP_BAD_REQUEST;
+            } else if($e instanceof TooManyRequestsHttpException) {
+                $return->message = $e->getMessage();
+                $return->code = Response::HTTP_TOO_MANY_REQUESTS;
+            } else if($e instanceof UnauthorizedHttpException) {
+                $return->message = $e->getMessage();
+                $return->code = Response::HTTP_UNAUTHORIZED;
+            } else if($e instanceof UnprocessableEntityHttpException) {
+                $return->message = $e->getMessage();
+                $return->code = Response::HTTP_UNPROCESSABLE_ENTITY;
             }
+
+            return response()->json(
+                ['message' => $return->message],
+                $return->code
+            );
         });
     })->create();
