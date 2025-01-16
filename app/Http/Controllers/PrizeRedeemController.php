8a0d2baaa603154f500345c0e34eef85d3b8fa0f
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-01-15 22:44:41 -0300

    updated some routes

--- app/Http/Controllers/PrizeRedeemController.php
+++ app/Http/Controllers/PrizeRedeemController.php
@@ -13,23 +13,8 @@ class PrizeRedeemController extends BaseController
 		parent::__construct($service);
 	}
 
-	public function index()
-	{
-		return $this->defaultResponse($this->service->getAllRecords());
-	}
-
 	public function store(PrizeRedeemRequest $request)
 	{
 		return $this->defaultResponse($this->service->saveRecord($request->getData()), Response::HTTP_CREATED);
-	}
-
-	public function show(string $id)
-	{
-		return $this->defaultResponse($this->service->findRecord($id));
-	}
-
-	public function update(PrizeRedeemRequest $request, string $id)
-	{
-		return $this->defaultResponse($this->service->updateRecord($id, $request->getData()), Response::HTTP_CREATED);
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -26,24 +26,12 @@ use Illuminate\Support\Facades\Route;
 		Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
 	});
 
-	/**
-	 * PRODUCTS
-	 */
-	Route::prefix(ProductEnum::ROUTE_PREFIX)->group(function () {
-		Route::get('/', [ProductController::class, 'index'])->name('products.index');
-		Route::post('/', [ProductController::class, 'store'])->name('products.store');
-		Route::put('/{id}', [ProductController::class, 'update'])->name('products.update');
-		Route::get('/{id}', [ProductController::class, 'show'])->name('products.show');
-	});
-
     /**
 	 * TRANSACTIONS
 	 */
 	Route::prefix(TransactionEnum::ROUTE_PREFIX)->group(function () {
 		Route::get('/', [TransactionController::class, 'index'])->name('transactions.index');
 		Route::post('/', [TransactionController::class, 'store'])->name('transactions.store');
-		Route::put('/{id}', [TransactionController::class, 'update'])->name('transactions.update');
-		Route::get('/{id}', [TransactionController::class, 'show'])->name('transactions.show');
 	});
 
     /**
@@ -57,9 +45,6 @@ use Illuminate\Support\Facades\Route;
 	 * PRIZE REDEEMS
 	 */
 	Route::prefix(PrizeRedeemEnum::ROUTE_PREFIX)->group(function () {
-		Route::get('/', [PrizeRedeemController::class, 'index'])->name('prize_redeems.index');
-		Route::post('/', [PrizeRedeemController::class, 'store'])->name('prize_redeems.store');
-		Route::put('/{id}', [PrizeRedeemController::class, 'update'])->name('prize_redeems.update');
-		Route::get('/{id}', [PrizeRedeemController::class, 'show'])->name('prize_redeems.show');
+		Route::post('/store', [PrizeRedeemController::class, 'store'])->name('prize_redeems.store');
 	});
 // });
